71299ef8969bfe825051c2ff7bb913331542b1ae
12177330 <12177330@192.168.56.1>
2022-09-26 14:07:19 +1000

    created repository

---
+++ EftposMachine/src/main/java/com/mycompany/eftposmachine/EftposMachine.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eftposmachine;
+
+/**
+ *
+ * @author 12177330
+ */
+public class EftposMachine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
