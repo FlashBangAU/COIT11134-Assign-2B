06d455f3ab352ed70bf0cff1c3de3ad85588340c
zaccannnon2613 <zaccannon2003@gmail.com>
2022-10-03 14:19:20 +1000

    Controller Classes added

---
+++ EftposMachine/src/main/java/com/mycompany/eftposmachine/FXMLOrderHistoryController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author zacca
+ */
+public class FXMLOrderHistoryController {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+}
