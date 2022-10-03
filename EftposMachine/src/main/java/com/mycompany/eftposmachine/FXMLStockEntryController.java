7fe60b71f8f34aa7741d0de44f05d64694f5f053
12177330 <12177330@192.168.56.1>
2022-10-03 15:29:47 +1000

    added controller files, only has exit actions in all controllers except menu.

--- EftposMachine/src/main/java/com/mycompany/eftposmachine/FXMLStockEntryController.java
+++ EftposMachine/src/main/java/com/mycompany/eftposmachine/FXMLStockEntryController.java
@@ -70,3 +70,19 @@ public class FXMLStockEntryController implements Initializable {
     }    
     
 }
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author zacca
+ */
+public class FXMLStockEntryController {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+}
